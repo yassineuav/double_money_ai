e27736bf39f5223cbc422a940d5293c061a6a073
Yassine Khaidouch <91646673+yassineuav@users.noreply.github.com>
2023-09-03 13:44:54 +0000

    add button

--- double_it/app/page.tsx
+++ double_it/app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
           <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
             Your invest Amount
           </span>
-          <input type="text" name="amount" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="your amount" />
+          <input type="text" name="amount" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="your invest" />
           </label>
         </div>
         <div>
